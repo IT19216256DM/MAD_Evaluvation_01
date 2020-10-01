8cb78aeeb0a43ffbc8a68826043e8bc8c5e0acd6
Dell <dewmini970915@gmail.com>
2020-10-01 14:23:42 +0530

    MAD comment 25

--- app/src/main/java/com/example/orderus/MainActivity4.java
+++ app/src/main/java/com/example/orderus/MainActivity4.java
@@ -49,7 +49,7 @@ public class MainActivity4 extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
                                    @Override
                                    public void onClick(View view) {
-                                       dbRef = FirebaseDatabase.getInstance().getReference().child("Employee");
+                                       dbRef = FirebaseDatabase.getInstance().getReference().child("Employee").child("Emp1");
                                        dbRef.addValueEventListener(new ValueEventListener() {
                                            @Override
                                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
