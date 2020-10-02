773eb2b71d0b40abc3250a4c91379525ba6fd534
Dell <dewmini970915@gmail.com>
2020-10-02 13:53:19 +0530

    MAD comment 27

--- app/src/main/java/com/example/orderus/SingIn.java
+++ app/src/main/java/com/example/orderus/SingIn.java
@@ -1,5 +1,61 @@
 package com.example.orderus;
 
 public class SingIn {
+    private String fname;
+    private String lname;
+    private String passwrd;
+    private Integer confpass;
+    private String   email;
+    private Integer  mobile;
 
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getPasswrd() {
+        return passwrd;
+    }
+
+    public void setPasswrd(String passwrd) {
+        this.passwrd = passwrd;
+    }
+
+    public Integer getConfpass() {
+        return confpass;
+    }
+
+    public void setConfpass(Integer confpass) {
+        this.confpass = confpass;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(Integer mobile) {
+        this.mobile = mobile;
+    }
+
+    public SingIn() {
+    }
 }
